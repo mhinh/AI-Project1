5339e6dd854f2a08d6800cbb0513d28d9a3dfb0e
My Tien Hinh <tienhinh@gmail.com>
2020-03-28 23:46:12 +1100

    add moves func still buggy

--- search/__main__.py
+++ search/__main__.py
@@ -4,25 +4,22 @@ import collections
 from search.util import print_move, print_boom, print_board
 
 
-def main():
-    with open(sys.argv[1]) as file:
-        data = json.load(file)
+
 
     # TODO: find and print winning action sequence
 
 def boom(current_state, coord):
     new_state = current_state.copy()
-    for team in new_state:
-        for member in team:
-            if member[1:3] == coord:
-                del member
-    xmin = coord[1]-1
-    xmax = coord[1]+2
-    ymin = coord[2]-1
-    ymax = coord[2]+2
-    for x in range(xmin, xmax):
-        for y in range(ymin, ymax):
-            new_state = boom(new_state, [x,y])
+    for member in new_state["white"]:
+        if member[1:3] == coord:
+            new_state["white"].remove(member)
+            xmin = coord[0]-1
+            xmax = coord[0]+2
+            ymin = coord[1]-1
+            ymax = coord[1]+2
+            for x in range(xmin, xmax):
+                for y in range(ymin, ymax):
+                    new_state = boom(new_state, [x,y])
 
     return new_state
 
@@ -31,36 +28,57 @@ def move(current_state, direction, coord):
     for white_member in new_state["white"]:
         if white_member[1:3] == coord:
             if direction == "left":
-                white_member[1] = white_member[1] - 1
+                white_member[1] -= 1
             if direction == "right":
-                white_member[1] = white_member[1] + 1
+                white_member[1] += 1
             if direction == "up":
-                white_member[2] = white_member[2] + 1
+                white_member[2] += 1
             if direction == "down":
-                white_member[2] = white_member[2] - 1
+                white_member[2] -= 1
     return new_state
 
-def all_moves(queue, coord, state):
-    queue.append(move(state, "left", coord))
-    queue.append(move(state, "right", coord))
-    queue.append(move(state, "up", coord))
-    queue.append(move(state, "down", coord))
-    queue.append(boom(state, coord))
+def add_all_moves(queue, path, coord, state):
+    if coord[0] >= 0:
+        queue.append(path + [move(state, "left", coord)])
+
+    if coord[0] <= 7:
+        queue.append(path + [move(state, "right", coord)])
+
+    if coord[1] <= 7:
+        queue.append(path + [move(state, "up", coord)])
+
+    if coord[1] >= 0:
+        queue.append(path + [move(state, "down", coord)])
+
+    queue.append(path + [boom(state, coord)])
 
 class Node:
     def __init__(self, state, children):
         self.state = state
         self.children = None
 
-def bfs(start_state, goal_state):
-    state_node = Node(start_state)
-    queue = collections.deque([start_state])
+def bfs(start_state):
+    queue = collections.deque([[start_state]])
     while queue:
-        state = queue.popleft()
-        if state == goal_state:
-            return
-        for white_member in state["white"]:
-            all_moves(queue, white_member[1:3], state)
+        path = queue.popleft()
+        current_state = path[-1]
+        if len(current_state["black"]) == 0:
+            return path
+        for white_member in current_state["white"]:
+            add_all_moves(queue, path, white_member[1:3], current_state)
+
+def main():
+    with open(sys.argv[1]) as file:
+        data = json.load(file)
+        path = bfs(data)
+        print(path)
+        #queue = collections.deque([[data]])
+        #path = queue.popleft()
+        #print("PATH")
+        #print(path)
+        #current_state = path[-1]
+        #for white_member in current_state["white"]:
+        #    add_all_moves(queue, path, white_member[1:3], current_state)
 
 
 if __name__ == '__main__':
